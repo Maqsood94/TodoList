64bef36ad9701f509a6c8affb163d5d5d126b13f
Maqsood Ahmed <maqsoodahmed1994@gmail.com>
2017-05-13 06:39:27 -0700

    TodoList implemented using HTML , CSS , JavaScript

--- myscript.js
+++ myscript.js
@@ -1,19 +1,61 @@
-    //function doAction(value)
-    //{
-    //    // Don't really have anything to set...just show the value
-    //    //alert(value);
-    //}
+var input = document.getElementById("input");
+
+function addItem(){
+    var ul = document.getElementById("Todolist");
+    var li = document.createElement("li");
+    var p = document.createElement("p");
+    var del = document.createElement("img");
+    var up = document.createElement("img");
+    var chkBox=document.createElement("input");
+
+    del.setAttribute("src","61391.png");
+    del.setAttribute("onclick", "deleteItem()");
+
+    up.setAttribute("src","update-icon.png");
+    up.setAttribute("onclick","updateItem()");
+
+    p.innerText=input.value;
+    chkBox.setAttribute("type","checkbox");
+    chkBox.setAttribute("onclick","Check()");
+
+    li.appendChild(chkBox);
+    li.appendChild(p);
+    li.appendChild(del);
+    li.appendChild(up);
+    ul.appendChild(li);
+
+ }
+function Check() {
+//console.log(event);
+if(event.target.checked)
+{
+    var target = event.target;
+    i = target.parentElement.children[1];
+    i.setAttribute("class","para");
+
+}
+else
+{
+
+    i.setAttribute("class"," ");
+}
 
 
-    //function getElements() {
-    //    var x=document.getElementsByName("myInput")
-    //    alert(x.length + " elements!")
-    //}
+}
+
+function updateItem(){
+
+    var target = event.target;
+    var i;
+    i = target.parentElement.children[0];
+    console.log(i);
+    i.innerText = input.value;
 
 
-    function myFunction() {
-        var node = document.createElement("LI");
-        var textnode = document.createTextNode("Water");
-        node.appendChild(textnode);
-        document.getElementById("myList").appendChild(node);
+}
+  function deleteItem(){
+
+   var x = event.target.parentElement;
+      x.remove();
+
     }
